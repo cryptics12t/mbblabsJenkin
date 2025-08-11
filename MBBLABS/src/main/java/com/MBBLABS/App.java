4c009c4874f00fbdda312cb07f424a593db77492
cryptics12t <142624002+cryptics12t@users.noreply.github.com>
2025-08-11 09:01:22 +0000

    This my first commit.

---
+++ MBBLABS/src/main/java/com/MBBLABS/App.java
@@ -0,0 +1,13 @@
+package com.MBBLABS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
