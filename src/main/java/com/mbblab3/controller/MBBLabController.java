481a0383d33115e6c201592ef7e2e4de8e9c56fa
Mbblabs <142624002+cryptics12t@users.noreply.github.com>
2025-08-14 09:49:10 +0000

    commit

--- src/main/java/com/mbblab3/controller/MBBLabController.java
+++ src/main/java/com/mbblab3/controller/MBBLabController.java
@@ -16,13 +16,13 @@ public class MBBLabController {
 		
 		List<Employee> list =new ArrayList<Employee>();
 			Employee e1=new Employee();
-			e1.setName("Rajesh");
+			e1.setName("Smaranika");
 			e1.setPassword("abcd");
 			e1.setEmail("abc@gmail.com");
 			e1.setAddress("Bangalore");
 			
 			Employee e2=new Employee();
-			e2.setName("Rajesh1");
+			e2.setName("Simi");
 			e2.setPassword("abcd1");
 			e2.setEmail("abc1@gmail.com");
 			e2.setAddress("Bangalore1");
